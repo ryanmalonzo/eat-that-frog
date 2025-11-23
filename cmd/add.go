04047c4647f625e8d609d12161289bebff80d17a
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-11-23 16:12:18 +0100

    feat: implement `add` command

---
+++ cmd/add.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/ryanmalonzo/eat-that-frog-cli/internal/db"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add [task]",
+	Short: "Add a candidate frog",
+	Long:  `Add a new task to your list of candidate frogs to eat.`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		task := args[0]
+		return db.AddTask(task)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
